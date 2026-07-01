0e7d09032d3a6327554585a80879683ffd222d85
Nguyễn Văn Sĩ <96609459+singuyenvan0206@users.noreply.github.com>
2026-07-01 08:56:13 +0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'mtba-backend',
       cwd: './code/backend',
-      script: 'dist/main.js',
+      script: 'dist/src/main.js',
       instances: 1,
       autorestart: true,
       watch: false,
