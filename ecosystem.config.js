50c2211a3a22413bc8daa3ccf728954ad5c1bc18
Nguyễn Văn Sĩ <96609459+singuyenvan0206@users.noreply.github.com>
2026-07-01 08:50:09 +0700

    Create ecosystem.config.js

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [
+    {
+      name: 'mtba-backend',
+      cwd: './code/backend',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        PORT: 3001,
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'mtba-frontend',
+      cwd: './code/frontend',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+};
